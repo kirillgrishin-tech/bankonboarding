6c16f69a3ed4152da256541c26577338cf011faa
Гришин Кирилл Андреевич <KAGrishin@alfabank.ru>
2025-03-30 13:53:58 +0300

    feature/task-2: fix

--- src/main/java/ru/alfabank/practice/kagrishin/bankonboarding/schedule/ScheduledTasks.java
+++ src/main/java/ru/alfabank/practice/kagrishin/bankonboarding/schedule/ScheduledTasks.java
@@ -1,4 +1,4 @@
-package ru.alfabank.practice.kagrishin.bankonboarding.shedule;
+package ru.alfabank.practice.kagrishin.bankonboarding.schedule;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
